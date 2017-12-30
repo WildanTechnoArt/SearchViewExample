b0d5e62d9921be2c6e2fc785dd939f1deed74787
WildanTechnoArt <wildantechnoart77@gmail.com>
2017-12-30 20:21:42 +0700

    Initial commit

---
+++ app/src/main/java/android/cianjur/developer/net/searchviewexample/MainActivity.java
@@ -0,0 +1,53 @@
+package android.cianjur.developer.net.searchviewexample;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.support.v7.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView Hasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Hasil = findViewById(R.id.outout);
+    }
+
+    //Code Program pada Method dibawah ini akan Berjalan saat Option Menu Dibuat
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Memanggil/Memasang menu item pada toolbar dari layout menu_bar.xml
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_bar, menu);
+        MenuItem searchIem = menu.findItem(R.id.search);
+        final SearchView searchView = (SearchView) searchIem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Hasil.setText("Hasil Pencarian: "+query);
+                Toast.makeText(getApplicationContext(),query, Toast.LENGTH_SHORT).show();
+
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return true;
+    }
+}
